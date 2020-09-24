ba842dbf22bc5dc9d708259e7715112e01ae9b38
Kabaye <svkulich@gmail.com>
2020-09-24 18:46:40 +0300

    Initial commit

---
+++ sn-client/src/main/java/edu/bsu/sn/client/SnClientApplication.java
@@ -0,0 +1,13 @@
+package edu.bsu.sn.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnClientApplication.class, args);
+	}
+
+}
---
+++ sn-server/src/main/java/edu/bsu/sn/server/SNServerApplication.java
@@ -0,0 +1,13 @@
+package edu.bsu.sn.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SNServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SNServerApplication.class, args);
+	}
+
+}
